bcd7da9d16457c193ee23605f5a046723ff4d245
Allaa <akhayat21@yahoo.com>
2019-06-28 16:34:41 -0400

    added function to find how many ways to split tip/bill

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,22 +1,32 @@
 function increase() {
-   var val = document.getElementById("split").innerHTML;
-   if (val === "no"){
-    document.getElementById("split").innerHTML = 2;
-   } else {
-    document.getElementById("split").innerHTML = parseInt(document.getElementById("split").innerHTML) + 1 
-   }
+    var val = document.getElementById("split").innerHTML;
+    if (val === "no") {
+        document.getElementById("split").innerHTML = 2;
+    } else {
+        document.getElementById("split").innerHTML = parseInt(document.getElementById("split").innerHTML) + 1;
+    }
 
 }
 
 function decrease() {
     var val = document.getElementById("split").innerHTML;
-    if (val === "no"){
+    if (val === "no") {
     } else if (val === "1") {
-    document.getElementById("split").innerHTML = "no"
+        val = "no";
     }
-    else
-    {
-     document.getElementById("split").innerHTML = parseInt(document.getElementById("split").innerHTML) - 1
+    else {
+        document.getElementById("split").innerHTML = parseInt(document.getElementById("split").innerHTML) - 1;
     }
- 
- }
+
+}
+
+function splitFinder() {
+    var split = document.getElementById("split").innerHTML;
+    if(split === "no"){
+        split = 0;
+        return split;
+    } else {
+        split = parseInt(document.getElementById("split").innerHTML);
+        return split;
+    }
+}
