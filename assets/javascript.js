bbe27fffac5a549ea25661ebbf6fd602a92afccc
Allaa <akhayat21@yahoo.com>
2019-06-29 13:05:51 -0400

    added materialize and fixed class names

--- assets/javascript.js
+++ assets/javascript.js
@@ -11,8 +11,8 @@ function increase() {
 function decrease() {
     var val = document.getElementById("split").innerHTML;
     if (val === "no") {
-    } else if (val === "1") {
-        val = "no";
+    } else if (val === "2") {
+        document.getElementById("split").innerHTML = "no";
     }
     else {
         document.getElementById("split").innerHTML = parseInt(document.getElementById("split").innerHTML) - 1;
@@ -23,10 +23,21 @@ function decrease() {
 function splitFinder() {
     var split = document.getElementById("split").innerHTML;
     if(split === "no"){
-        split = 0;
+        split = 1;
         return split;
     } else {
         split = parseInt(document.getElementById("split").innerHTML);
         return split;
     }
+}
+
+function calculate() {
+    var split = splitFinder();
+    var billTotal = parseInt(document.getElementById("total").value)
+    var tip = document.getElementById("tip").value.replace("%","")/100
+
+    var billEach = billTotal/split;
+    var tipTotal = billTotal*tip;
+    var tipEach = tipTotal/split;
+    var totalEach = billEach + tipEach; 
 }
